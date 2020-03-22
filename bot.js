4b7bd5416acbbacfc11402d433dcf3bb87253f53
Luka <noobtenluka@gmail.com>
2020-03-22 23:01:12 +0100

    Added a config file and an example for the config file.

--- bot.js
+++ bot.js
@@ -1,33 +1,35 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const config = require("./config.json");
 var fs = require("fs");
-var adminchannel;
+var welcomeChannel;
 
 client.on("ready", () => {
   console.log("Discord Bot started!");
-  adminchannel = client.channels.cache.get("689949212011528262");
+  welcomeChannel = client.channels.cache.get(config.welcomeChannelID);
 });
 
 client.on("message", msg => {
   // Command Hadling
 });
 
-
 client.on("guildMemberAdd", member => {
   try {
-
     //console.log(member.client.user.locale);
     let localeCode = member.user.locale;
-    if(!localeCode) {
+    if (!localeCode) {
       localeCode = "en-GB";
     }
-    let languageJSON = JSON.parse(fs.readFileSync("./assets/i18n/" + localeCode + ".json"));
-    member.send({embed: {color: 3447003, fields: languageJSON.welcome}});
-    adminchannel.send("Ein neuer Benutzer hat den Discord betreten: " + member.displayName);
+    let languageJSON = JSON.parse(
+      fs.readFileSync("./assets/i18n/" + localeCode + ".json")
+    );
+    member.send({ embed: { color: 3447003, fields: languageJSON.welcome } });
+    welcomeChannel.send(
+      "Ein neuer Benutzer hat den Discord betreten: " + member.displayName
+    );
   } catch (e) {
     console.error(e);
   }
 });
 
-
-client.login("<private key>");
+client.login(config.token);
