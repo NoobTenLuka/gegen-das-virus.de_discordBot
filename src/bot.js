9b0f898bc111cabf5a6aaeabf059ac5191992dc6
Luka <noobtenluka@gmail.com>
2020-03-24 13:10:32 +0100

    Modified eslint to use prettier

--- src/bot.js
+++ src/bot.js
@@ -1,31 +1,31 @@
-const Discord = require("discord.js");
-const Enmap = require("enmap");
-const fs = require("fs");
+const Discord = require('discord.js')
+const Enmap = require('enmap')
+const fs = require('fs')
 
-const client = new Discord.Client();
-const config = require("./config.json");
-client.config = config;
+const client = new Discord.Client()
+const config = require('./config.json')
+client.config = config
 
-fs.readdir("./events/", (err, files) => {
-    if (err) return console.error(err);
-    files.forEach(file => {
-        const event = require(`./events/${file}`);
-        const eventName = file.split(".")[0];
-        client.on(eventName, event.bind(null, client));
-    });
-});
+fs.readdir('./events/', (err, files) => {
+  if (err) return console.error(err)
+  files.forEach((file) => {
+    const event = require(`./events/${file}`)
+    const eventName = file.split('.')[0]
+    client.on(eventName, event.bind(null, client))
+  })
+})
 
-client.commands = new Enmap();
+client.commands = new Enmap()
 
-fs.readdir("./commands/", (err, files) => {
-    if (err) return console.error(err);
-    files.forEach(file => {
-        if (!file.endsWith(".js")) return;
-        const props = require(`./commands/${file}`);
-        const commandName = file.split(".")[0];
-        console.log(`Attempting to load command ${commandName}`);
-        client.commands.set(commandName, props);
-    });
-});
+fs.readdir('./commands/', (err, files) => {
+  if (err) return console.error(err)
+  files.forEach((file) => {
+    if (!file.endsWith('.js')) return
+    const props = require(`./commands/${file}`)
+    const commandName = file.split('.')[0]
+    console.log(`Attempting to load command ${commandName}`)
+    client.commands.set(commandName, props)
+  })
+})
 
-client.login(config.token);
+client.login(config.token)
